b013e78198190cf5dec1fd46976b134578e5d208
Nihal Kumarswamy <kumarnihal04@gmail.com>
2019-06-12 12:02:35 +0530

    final

--- Makefile
+++ Makefile
@@ -3,3 +3,6 @@ CXXFLAGS += --std=c++17 -Wall
 
 main:main.cpp
 	${CXX} ${CXXFLAGS} -o game main.cpp
+
+clean:
+	rm -f game
--- main.cpp
+++ main.cpp
@@ -1,13 +1,34 @@
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
 
 int main(){
-int x=5,y;	//Implement a random function(with seed) for x
+int x,y,input,correct=0,score=0;	//Implement a random function(with seed) for x
 
 std::cout<<"Welcome!\nChoose a difficulty\n(1)Easy(y=3)\n(2)Medium(y=6)\n(3)Hard(y=9)\n(4)Expert(y=12)\n";
 std::cin>>y;
 
+srand(time(NULL));
+x=rand()%10;
 y*=3;
 
+std::cout<<"Enter any number other than "<<x<<"\n";
 
+while(true){
+	std::cout<<"Enter your number: ";
+	std::cin>>input;
+	if(input==x){
+		std::cout<<"Hey! you were not supposed to enter "<<input<<"\n";
+		break;
+		}
+	else{
+		correct++;
+		std::cout<<"well done!\n";
+		}
+	if(correct==y){
+	score++;
+	correct=0;}
+	}
+std::cout<<"Your score: "<<score<<"\n";
 return 0;
 }
